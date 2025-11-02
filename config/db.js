619a937823071e821ec2c2766c0635ef7152b8ad
unknown <veliyathvarsha@gmail.com>
2025-11-02 17:38:00 +0530

    fix: added connectDB import in inngest config

--- config/db.js
+++ config/db.js
@@ -1,24 +1,25 @@
 import mongoose from "mongoose";
 
-let cached= global.mongoose
+let cached = global.mongoose;
 
-if (!cached) { 
-  cached =global.mongoose ={conn: null, promise :null}
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function connection(){
-  if (cached.conn){
-    return cached.conn
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
   }
-  if (!cached.promise){
-      const opts= {
-        bufferCommands: false
-      }
-      cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then(mongoose => {
-        return mongoose
-      })
-    }
-    cached.conn= await cached.promise
-    return cached.conn
+
+  if (!cached.promise) {
+    const opts = { bufferCommands: false };
+    cached.promise = mongoose
+      .connect(`${process.env.MONGODB_URI}/quickcart`, opts)
+      .then((mongoose) => mongoose);
   }
- export default connectDB
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectDB;
